2cd3ac1a795d22d506ad37569dbf388da092a1ce
田康康 <77710383+GitHub22520@users.noreply.github.com>
2026-02-05 16:35:30 +0800

    基本框架搭建

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,12 +1,17 @@
 <template>
-    <div>
-        首页
-        <!-- 二级路由出口 -->
-        <router-view></router-view>
-    </div>
+    <LayoutNav/>
+    <LayoutHeader/>
+
+    <router-view></router-view>
+    
+    <LayoutFooter/>
 </template>
 
 <script lang="ts" setup>
+// 导入基本页面框架
+import LayoutNav from './components/LayoutNav.vue';
+import LayoutHeader from './components/LayoutHeader.vue';
+import LayoutFooter from './components/LayoutFooter.vue';
 </script>
 
 <style scoped>
