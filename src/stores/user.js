f2971217186d27e734f4c6ea702a20a43ac65f21
田康康 <77710383+GitHub22520@users.noreply.github.com>
2026-02-11 15:12:14 +0800

    登录-pinia 数据持久化

--- src/stores/user.js
+++ src/stores/user.js
@@ -4,17 +4,23 @@ import { ref } from "vue";
 
 import { loginAPI } from "@/apis/user";
 
-export const userUserStore = defineStore("user", () => {
-  // 定义管理用户数据的 state
-  const userInfo = ref({});
-  // 定义获取接口数据的 action 函数
-  const getUserInfo = async ({ account, password }) => {
-    const res = await loginAPI({ account, password });
-    userInfo.value = res.result;
-  };
-  // 以对象的格式把 state 和 action 返回
-  return {
-    userInfo,
-    getUserInfo,
-  };
-});
+export const userUserStore = defineStore(
+  "user",
+  () => {
+    // 定义管理用户数据的 state
+    const userInfo = ref({});
+    // 定义获取接口数据的 action 函数
+    const getUserInfo = async ({ account, password }) => {
+      const res = await loginAPI({ account, password });
+      userInfo.value = res.result;
+    };
+    // 以对象的格式把 state 和 action 返回
+    return {
+      userInfo,
+      getUserInfo,
+    };
+  },
+  {
+    persist: true, // 开启数据持久化
+  },
+);
