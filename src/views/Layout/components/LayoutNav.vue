8c9a085243a3397d0d190bb7a3b2b9db31b98987
田康康 <77710383+GitHub22520@users.noreply.github.com>
2026-02-11 15:17:24 +0800

    登录-使用 token 适配 nav 模板

--- src/views/Layout/components/LayoutNav.vue
+++ src/views/Layout/components/LayoutNav.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { userUserStore } from '@/stores/user';
+const userStore = userUserStore()
 
 </script>
 
@@ -7,8 +9,8 @@
     <div class="container">
       <ul>
         <!-- 多模版渲染 -->
-        <template v-if="false">
-            <li><a href="javascript:;"><i class="iconfont icon-user"></i>周杰伦</a></li>
+        <template v-if="userStore.userInfo.token">
+            <li><a href="javascript:;"><i class="iconfont icon-user"></i>{{ userStore.userInfo.nickname }}</a></li>
             <li>
                 <el-popconfirm title="确认退出吗？" confirm-button-text="确认" cancel-button="取消">
                     <template #reference>
