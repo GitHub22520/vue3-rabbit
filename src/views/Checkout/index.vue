ee7244b5b6c3c1d00a2af1f24d9acb03c8ea489c
田康康 <77710383+GitHub22520@users.noreply.github.com>
2026-02-15 13:10:09 +0800

    地址切换-打开弹窗

--- src/views/Checkout/index.vue
+++ src/views/Checkout/index.vue
@@ -17,9 +17,35 @@ const getCheckInfo = async () => {
 onMounted(() => {
   getCheckInfo()
 })
+
+// 切换地址
+// 控制弹窗打开
+const showDialog = ref(false)
 </script>
 
 <template>
+  <!-- 弹窗部分 -->
+  <!-- 切换地址 -->
+  <el-dialog v-model="showDialog" title="切换收货地址" width="30%" center>
+    <div class="addressWrapper">
+      <div class="text item" 
+        v-for="item in checkInfo.userAddresses" :key="item.id">
+        <ul>
+          <li><span>收<i />货<i />人：</span>{{ item.receiver }} </li>
+          <li><span>联系方式：</span>{{ item.contact }}</li>
+          <li><span>收货地址：</span>{{ item.fullLocation + item.address }}</li>
+        </ul>
+      </div>
+    </div>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button>取消</el-button>
+        <el-button type="primary" @click="confirm">确定</el-button>
+      </span>
+    </template>
+  </el-dialog>
+  <!-- 添加地址 -->
+
   <div class="xtx-pay-checkout-page">
     <div class="container">
       <div class="wrapper">
@@ -116,25 +142,6 @@ onMounted(() => {
       </div>
     </div>
   </div>
-  <!-- 切换地址 -->
-  <el-dialog v-model="showDialog" title="切换收货地址" width="30%" center>
-    <div class="addressWrapper">
-      <div class="text item":class="{active: activeAddress.id === item.id}"  @click="switchAddress(item)" v-for="item in checkInfo.userAddresses"  :key="item.id">
-        <ul>
-        <li><span>收<i />货<i />人：</span>{{ item.receiver }} </li>
-        <li><span>联系方式：</span>{{ item.contact }}</li>
-        <li><span>收货地址：</span>{{ item.fullLocation + item.address }}</li>
-        </ul>
-      </div>
-    </div>
-    <template #footer>
-      <span class="dialog-footer">
-        <el-button>取消</el-button>
-        <el-button type="primary" @click="confirm">确定</el-button>
-      </span>
-    </template>
-  </el-dialog>
-  <!-- 添加地址 -->
 </template>
 
 <style scoped lang="scss">
